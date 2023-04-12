b673a888ad497cbe47d2b43b93d882aac6a95a6c
siddastic <siddhantgaurav2003@gmail.com>
2023-04-13 01:53:05 +0530

    create Element tree

--- src/api/graph/wire_graph.ts
+++ src/api/graph/wire_graph.ts
@@ -2,20 +2,49 @@ import { GraphBackground } from "./graph_background";
 import "../../styles/main.css";
 import { GraphContainer } from "./graph_container";
 
-export class WireGraph{
-    rootGraph! : HTMLDivElement;
-    graphBackground! : GraphBackground;
-    graphContainer! : GraphContainer;
-    constructor(element : HTMLDivElement){
+export class WireGraph {
+    rootGraph!: HTMLDivElement;
+    graphBackground!: GraphBackground;
+    graphContainer!: GraphContainer;
+    elementTree!: { nodeContainer: HTMLDivElement; svgContainer: SVGElement };
+    constructor(element: HTMLDivElement) {
         this.rootGraph = element;
-        this.rootGraph.classList.contains("wire-graph") ? void 0 : this.rootGraph.classList.add("wire-graph");
+        this.rootGraph.classList.contains("wire-graph")
+            ? void 0
+            : this.rootGraph.classList.add("wire-graph");
         this.init();
+        this.createElementTree();
     }
 
-    private init(){
-        // create element tree
+    private init() {
+        // init
         this.graphBackground = new GraphBackground(this.rootGraph);
         // append the graph container inside the background element so grid stays behind the container
-        this.graphContainer = new GraphContainer(this.graphBackground.background);
+        this.graphContainer = new GraphContainer(
+            this.graphBackground.background
+        );
+    }
+
+    private createElementTree() {
+        // creates elements required for graph functionality
+        this.elementTree = {
+            nodeContainer: document.createElement("div"),
+            svgContainer: document.createElementNS(
+                "http://www.w3.org/2000/svg",
+                "svg"
+            ),
+        };
+
+        // add classes to elements for styling
+        this.elementTree.nodeContainer.classList.add("node-container");
+        this.elementTree.svgContainer.classList.add("svg-container");
+
+        // append elements to graph container
+        this.graphContainer.graphContainer.appendChild(
+            this.elementTree.nodeContainer
+        );
+        this.graphContainer.graphContainer.appendChild(
+            this.elementTree.svgContainer
+        );
     }
 }
