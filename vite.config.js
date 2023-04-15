407695575c8040152d66b90bb1b1568c0aeb0705
siddastic <siddhantgaurav2003@gmail.com>
2023-04-16 00:49:10 +0530

    generate declaration files for build

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,31 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  build: {
-    minify: false,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'WireGraph',
-      // the proper extensions will be added
-      fileName: 'wires',
+    build: {
+        minify: false,
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, "src/main.ts"),
+            name: "WireGraph",
+            // the proper extensions will be added
+            fileName: "wires",
+        },
+        // rollupOptions: {
+        //   output: {
+        //     // Provide global variables to use in the UMD build
+        //     // for externalized deps
+        //     globals: {
+        //       // vue: 'WireGraph'
+        //     }
+        //   }
+        // },
     },
-    // rollupOptions: {
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       // vue: 'WireGraph'
-    //     }
-    //   }
-    // },
-    
-  },
-  plugins: [dts()],
-})
+    plugins: [
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+});
